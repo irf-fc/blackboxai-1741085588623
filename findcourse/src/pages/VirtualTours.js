a50987999f73b97c035c0f6ae7b9d986fd9fbff6
irf-fc <dr.m1.irfan@gmail.com>
2025-03-04 10:53:20 +0000

    Add findcourse/src/pages/VirtualTours.js 1741085600507

---
+++ findcourse/src/pages/VirtualTours.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const VirtualTours = () => {
+  return (
+    <div>
+      <h2>Virtual Campus Tours</h2>
+      {/* Virtual tours functionality will be implemented here */}
+      <p>Explore our virtual campus tours coming soon!</p>
+    </div>
+  );
+};
+
+export default VirtualTours;
